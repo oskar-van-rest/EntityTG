b1842a1b85af5a96874f3c5a8669f4c4f2a2cdc3
oskarvanrest <oskarvanrest@gmail.com>
2013-05-23 17:46:07 -0700

    switched to tmpl

--- org.spoofax.entitytg.textual/syntax/EntityTG.pp
+++ org.spoofax.entitytg.textual/syntax/EntityTG.pp
@@ -1,2 +1,13 @@
 %% Pretty printing table (see also EntityTG.generated.pp)
-[]
+[
+   Module              -- KW["module"] _1 _2,
+   Module.2:iter-star  -- _1,
+   Entity              -- KW["entity"] _1 KW["{\n"] _2 KW["   \n}"],
+   Entity.2:iter-star  -- _1,
+   DataType            -- KW["datatype"] _1,
+   Attribute           -- KW["attr"] _1 KW[":"] _2,
+   Reference           -- KW["ref"] _1 KW[":"] _2,
+   COMPLETION-Start    -- _1,
+   COMPLETION-Type     -- _1,
+   COMPLETION-Property -- _1
+]
--- org.spoofax.entitytg.textual/test/example.java
+++
@@ -1,6 +0,0 @@
-package example;
-
-class User {
-    
-}
-
